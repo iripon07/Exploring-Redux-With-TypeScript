db717322d7f47555ad4a2809409cbdfe53b02f6a
AsadulIslamRipon <mdasadulripon@gmail.com>
2023-12-12 15:49:50 +0600

    Connect component with redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
 
 function App() {
-  return <h1 className="bg-red-500">Tech Net</h1>
+  const {count} = useSelector((state: RootState) =>state.counter);
+  return (
+    <div className="mx-auto max-w-2xl h-auto">
+      <div className="flex justify-center items-center ">
+        <button className="p-10">Increment</button>
+        <div>{count}</div>
+        <button className="p-10">Decrement</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
